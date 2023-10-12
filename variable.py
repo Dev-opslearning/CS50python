2a0b369bf3aa3c9365286efbd8e9f27552be675f
Harshavardhana <harsha4688v@gmail.com>
2023-10-12 10:08:58 +0530

    2nd commit

--- variable.py
+++ variable.py
@@ -1,3 +1,51 @@
-name = input("what is your name:")
-print ("hello,"+name)
-print ("hello,",name)
+# name = input("what is your name:")
+# print ("hello,"+name)
+# print ("hello,",name)
+# print (f"hello, {name}")
+
+
+
+# a=b=c=d= 20
+# print(b)
+
+# name = "harsha"
+# a = 23
+# b = 2.4
+# c = 2+3j
+# d = True
+# sum = (name,a,b,c,d)
+# print(type(sum))
+
+
+# a = 23
+# b = "23"
+# c = 2+5j
+# print (type(a))
+# print(type(b))
+# print (type(c))
+
+# a = 12.3
+# b = int(a)
+# print (b)
+# print (type(b))
+
+# a = 12.3
+# b = str(a)
+# print (b)
+# print (type(b))
+
+# a = (input("first number :"))
+# b = (input("Second number :"))
+# if a == b :
+#     print("a == b is True")
+# else:
+#     print("a == b is False")
+
+a=20
+b=40
+if a>b:
+    print("a is greater")
+elif a<b:
+    print("a is smallest")
+else:
+    print("b is greater")
